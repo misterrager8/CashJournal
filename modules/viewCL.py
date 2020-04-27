40d7137699bc14d0cfaa20885f6950819bb66fee
Chemllei Joseph <chemllei0304@gmail.com>
2020-04-26 21:49:42 -0400

    cl view changed

--- modules/viewCL.py
+++ modules/viewCL.py
@@ -1,6 +1,10 @@
 import sys
 
-main_options = ["Option 0", "Option 1", "Option 2", "Option 3", "Exit"]
+from modules.ctrla import Ctrla
+from modules.model import Transx
+
+main_options = ["View All", "Add", "Monthly Statement", "Option 3", "Exit"]
+tc = Ctrla()
 
 
 class Mainmenu:
@@ -11,11 +15,17 @@ class Mainmenu:
 
             if main_options[choice] == "Exit":
                 sys.exit()
-            elif main_options[choice] == "Option 0":
-                print("Option 0")
-            elif main_options[choice] == "Option 1":
-                print("Option 1")
-            elif main_options[choice] == "Option 2":
+            elif main_options[choice] == "View All":
+                tc.view_all_items()
+            elif main_options[choice] == "Add":
+                desc = raw_input("Description? ")
+                amount = raw_input("Amount? ")
+                date_approved = raw_input("Date? ")
+                transx_type = raw_input("Type? ")
+
+                x = Transx(None, desc, amount, date_approved, transx_type)
+                tc.add_item(x)
+            elif main_options[choice] == "Monthly Statement":
                 print("Option 2")
             elif main_options[choice] == "Option 3":
                 print("Option 3")
