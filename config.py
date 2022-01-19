a883e07bb77b5b5ce3637c926021fecc65d65d11
Chemllei Joseph <chemllei0304@gmail.com>
2022-01-19 03:26:07 -0500

    set pool_recycle

--- config.py
+++ config.py
@@ -9,3 +9,4 @@ ENV = os.getenv("flask_env")
 SQLALCHEMY_DATABASE_URI = os.getenv("db_url")
 SECRET_KEY = os.getenv("secret_key")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_ENGINE_OPTIONS = {'pool_recycle': 60}
